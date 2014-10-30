057061611d5d5a92b326871e103f131146d25bde
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2014-10-30 14:45:57 +0100

    show actual images in search results

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,5 +11,5 @@ angular.module('blloonSearchApp')
   .controller('MainCtrl', function ($scope, Book) {
     $scope.$watch('query', function (query) {
       $scope.books = Book.query({ query: query });
-    })
+    });
   });
---
+++ app/scripts/directives/background-image.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name blloonSearchApp.directive:backgroundImage
+ * @description
+ * # backgroundImage
+ */
+angular.module('blloonSearchApp')
+  .directive('backgroundImage', function ($document) {
+    return {
+      restrict: 'A',
+      scope: {
+        backgroundImage: '='
+      },
+      link: function(scope, element) {
+        scope.$watch('backgroundImage', function () {
+          var src = scope.backgroundImage;
+          if (src) {
+            var img = $document[0].createElement('img');
+            img.onload = function() {
+              element.css({
+                backgroundImage: 'url(' + this.src + ')',
+                backgroundSize: 'cover'
+              });
+              element.addClass('loaded');
+            };
+            img.src = src;
+          }
+        });
+      }
+    };
+  });
